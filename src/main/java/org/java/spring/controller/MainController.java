64779ebe087c582d625f73031c1984c4ead061e9
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-12-04 12:37:08 +0100

    complete valdiation

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -6,9 +6,14 @@ import org.java.spring.db.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -40,5 +45,30 @@ public class MainController {
 		model.addAttribute("pizza", pizza);
 		
 		return "single";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model)  {
+		
+		model.addAttribute("pizza", new Pizza());
+		
+		return "create";
+	}
+	
+	@PostMapping("/pizzas/create")
+	public String store(
+			@Valid @ModelAttribute Pizza formPizza,
+			BindingResult bindingResult,
+			Model model){
+		
+		if(bindingResult.hasErrors()) {
+		     model.addAttribute("pizza", formPizza);
+			return "create";
+			}
+		
+		pizzaService.save(formPizza);
+		int savedPizza = formPizza.getId();
+		
+		 return "redirect:/pizzas/" + savedPizza;
 	}
 }
--- src/main/java/org/java/spring/db/pojo/Pizza.java
+++ src/main/java/org/java/spring/db/pojo/Pizza.java
@@ -1,10 +1,16 @@
 package org.java.spring.db.pojo;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Pizza {
@@ -14,13 +20,21 @@ public class Pizza {
 	private int id;
 	
 	@Column(length = 64)
+	@Length(min = 3, max = 60, message = "name must be between 3 and 60 characters")
+	@NotEmpty(message = "name can't be empty")
 	private String name;
 	
 	@Column(columnDefinition = "TEXT")
+	@Length(min = 3, max = 1500, message = "description must be between 3 and 1500 characters")
+	@NotEmpty(message = "description can't be empty")
 	private String description;
 	
 	@Column(columnDefinition = "TEXT")
 	private String image;
+	
+	@NotNull(message = "price can't be empty")
+	@Min(value = 1, message = "the price must be greater than 0")
+	@Max(value = 100, message = "the price must be lower than 100")
 	private double price;
 	
 	public Pizza() {}
